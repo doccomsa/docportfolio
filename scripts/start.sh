16d38b90649c7f4d129b11c004e87793fbb97168
doccomsa <newcomsa@nate.com>
2025-02-24 17:56:41 +0900

    deploy 4

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,20 +11,26 @@ APP_LOG="$LOG_PATH/application.log"
 ERROR_LOG="$LOG_PATH/error.log"
 DEPLOY_LOG="$LOG_PATH/0_deploy.log"
 
+# 현재 날짜/시간정보를 변수에 저장.
 NOW_DATETIME=$(date "+%Y-%m-%d-%aT%T")
 
+# $LOG_PATH 변수명에 폴더가 존재하지 않으면 폴더생성
 if [ ! -d $LOG_PATH ]; then
     mkdir $LOG_PATH
 fi
 
+# echo "문자열" 화면에 출력
 # build 파일 복사
 echo "$NOW_DATETIME > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 
+# 폴더변경
 cd $PROJECT_FULL_PATH
 
+# target 폴더안에 있는 jar파일을 step1 으로 복사
 cp $PROJECT_FULL_PATH/ezenshop/target/*.jar $JAR_FILE
 #sudo cp /home/ec2-user/app1/step1/ezenshop/target/ezenshop-1.0.0.jar.jar ezenshop-1.0.0.jar
 
+# 폴더변경
 cd $PROJECT_FULL_PATH
 
 
@@ -44,6 +50,7 @@ fi
 #nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app1/application-prod-db.properties -Dspring.profiles.active=prod $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 # ec2 80 port 는 sudo권한사용
+# 스프링 부트 프로젝트 java -jar ezenshop-1.0.0.jar 실행
 sudo nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app1/application-real.properties $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 sleep 30s
